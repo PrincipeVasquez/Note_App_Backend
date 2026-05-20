505064e4204b026019daa5ddccfb24453efea059
Principe Alejandro Vasquez Tenazoa <alejandro.vasquez@mecolabora.com.pe>
2026-05-20 10:23:56 -0500

    Se puso como puerto alternativo el puerto 3001

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use("/api/notes", notesRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
